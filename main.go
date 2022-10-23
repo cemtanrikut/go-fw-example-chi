614395d914a7cd7cf882273d51ea428baa136155
cemtanrikut <cemtanrikut@gmail.com>
2022-10-23 22:39:32 +0300

    Add Get router with parameter

--- main.go
+++ main.go
@@ -11,16 +11,24 @@ func main(){
 	router := chi.NewRouter()
 
 	router.Get("/", MainHandler)
+	router.Get("/api/{parameter}", GetWithParamHandler)
 
-	err := http.ListenAndServe(":8080", router); err != nil{
+	err := http.ListenAndServe(":8080", router); err != nil {
 		log.Println(err)
 	}
 
 }
 
-func MainHandler(w http.ResponseWriter, r *http.Request){
-	_, err := w.Write([]byte("Hello from Chi Router !")); err != nil{
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello from Chi Router !")); err != nil {
 		log.Println(err)
 	}
 
+}
+
+func GetWithParamHandler(w http.ResponseWriter, r *http.Request) {
+	param := chi.URLParam(r, "parameter")
+	_, err := w.Write([]byte("Hello with parameter: " + param)); err != nil {
+		log.Println(err)
+	}
 }
