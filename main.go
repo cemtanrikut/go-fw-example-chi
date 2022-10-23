d8975a082f037a09edae8dc4e7cde24859fd4330
cemtanrikut <cemtanrikut@gmail.com>
2022-10-23 22:53:36 +0300

    Add post router

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main(){
 
 	router.Get("/", MainHandler)
 	router.Get("/api/{parameter}", GetWithParamHandler)
+	router.Post("/api/add", PostHandler)
 
 	err := http.ListenAndServe(":8080", router); err != nil {
 		log.Println(err)
@@ -21,18 +22,33 @@ func main(){
 
 }
 
+// [GET] localhost:8080/
 func MainHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	_, err := w.Write([]byte("Hello from Chi Router !")); err != nil {
 		log.Println(err)
 	}
 
 }
 
+// [GET] localhost:8080/api/{parameter}
 func GetWithParamHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	param := chi.URLParam(r, "parameter")
 	_, err := w.Write([]byte("Hello with parameter: " + param)); err != nil {
 		log.Println(err)
 	}
+}
+
+// [POST] localhost:8080/api/add
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	_, err := w.Write([]byte("Hello from Chi Router [POST] !")); err != nil {
+		log.Println(err)
+	}	
 }
 
 // Chi Middleware
