1f0e6d093320b542c18210cbb57bfa03ca6901ab
cemtanrikut <cemtanrikut@gmail.com>
2022-10-23 22:22:17 +0300

    Add Get route and library

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cemtanrikut/go-fw-example-chi
 
 go 1.18
+
+require github.com/go-chi/chi v1.5.4
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import(
+	"github.com/go-chi/chi"
+	"log"
+	"net/htpp"
+)
+
 func main(){
-	
+	// Create new router
+	router := chi.NewRouter()
+
+	router.Get("/", MainHandler)
+
+	err := http.ListenAndServe(":8080", router); err != nil{
+		log.Println(err)
+	}
+
+}
+
+func MainHandler(w http.ResponseWriter, r *http.Request){
+	_, err := w.Write([]byte("Hello from Chi Router !")); err != nil{
+		log.Println(err)
+	}
+
 }
